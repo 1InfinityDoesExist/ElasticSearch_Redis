7fd49dcaa49ee3758de1e7e994fc1f5be474ee9d
1InfinityDoesExist <patelavinash508@gmail.com>
2021-09-11 00:32:18 +0545

    seach api added

--- src/main/java/com/example/erk/ElasticSearchAndRedisApplication.java
+++ src/main/java/com/example/erk/ElasticSearchAndRedisApplication.java
@@ -1,19 +1,23 @@
 package com.example.erk;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableCaching
-@EnableFeignClients
 @SpringBootApplication
 public class ElasticSearchAndRedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElasticSearchAndRedisApplication.class, args);
+
+		String encoded = "{&quot;groupId&quot;:&quot;60228201e85b8300067656ff&quot;,&quot;schemaId&quot;:&quot;6013fca9a5a43c000638b0c0&quot;,&quot;tenantId&quot;:&quot;59ce808c99298e1e06660c09&quot;,&quot;groupDefinition&quot;:&quot;SELECT t0.&quot;id&quot; AS id FROM t_6013fca9a5a43c000638b0c0_t t0 WHERE (t0.&quot;entity.status&quot; LIKE 'running')&quot;\n"
+				+ "\n";
+		String decoded = StringEscapeUtils.unescapeHtml3(encoded);
+		System.out.println(decoded);
 	}
 
 }
